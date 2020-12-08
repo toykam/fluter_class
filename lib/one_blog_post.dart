25f6bd09bd4e33a6eb92ddea842d601e7a605a56
toykam <mwork063@gmail.com>
2020-12-08 16:33:24 +0100

    First commit

--- lib/one_blog_post.dart
+++ lib/one_blog_post.dart
@@ -1,18 +1,10 @@
 import 'package:flutter/material.dart';
 
-class OneBlogPost extends StatefulWidget {
-  @override
-  _OneBlogPostState createState() => _OneBlogPostState();
-}
-
-class _OneBlogPostState extends State<OneBlogPost> {
-
-  bool liked = false;
-
+class OneBlogPost extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 3,
+      elevation: 4,
       child: Container(
         margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
         height: MediaQuery.of(context).size.height * .20,
@@ -20,38 +12,31 @@ class _OneBlogPostState extends State<OneBlogPost> {
           children: [
             Positioned(
               top: 0, right: 0, left: 0,
-              child: InkWell(
-                onDoubleTap: () {
-                  setState(() {
-                    liked = !liked;
-                  });
-                },
-                child: Container(
-                  child: Row(
-                    children: [
-                      Container(
-                        child: Image.asset('assets/images/image1.jpg', height: 200, width: 100, fit: BoxFit.cover,),
-                      ),
-                      SizedBox(width: 10,),
-                      Expanded(
-                        child: Container(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text("Post Title", style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 24
-                              ),),
-                              SizedBox(height: 10,),
-                              Text(
-                                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-                                textAlign: TextAlign.justify,
-                              ),
-                            ],
-                          ),
+              child: Container(
+                child: Row(
+                  children: [
+                    Container(
+                      child: Image.asset('assets/images/image1.jpg', height: 200, width: 100, fit: BoxFit.cover,),
+                    ),
+                    SizedBox(width: 10,),
+                    Expanded(
+                      child: Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Post Title", style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 24
+                            ),),
+                            SizedBox(height: 10,),
+                            Text(
+                              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                              textAlign: TextAlign.justify,
+                            ),
+                          ],
                         ),
-                      )
-                    ],
-                  ),
+                      ),
+                    )
+                  ],
                 ),
               ),
             ),
@@ -63,14 +48,7 @@ class _OneBlogPostState extends State<OneBlogPost> {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    InkWell(
-                      onTap: () {
-                        setState(() {
-                          liked = !liked;
-                        });
-                      },
-                      child: Icon(Icons.thumb_up, color: liked ? Colors.red : Colors.black,)
-                    ),
+                    Icon(Icons.thumb_up),
                     Icon(Icons.comment),
                     Icon(Icons.share),
                   ],
